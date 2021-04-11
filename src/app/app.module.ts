bc4121758cf00e196d91e22b5420bbb180192464
Elton_Lai <lai.elton@gmail.com>
2021-04-11 14:21:28 +0800

    英雄編輯器

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+import { FormsModule } from '@angular/forms';
+import { HeroeszzzComponent } from './heroeszzz/heroeszzz.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeszzzComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
