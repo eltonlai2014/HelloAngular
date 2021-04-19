c361597878fff3795cd6879547b3677e26758228
Elton_Lai <lai.elton@gmail.com>
2021-04-19 21:57:36 +0800

    add route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-
 import { FormsModule } from '@angular/forms';
+
+import { HeroesComponent } from './heroes/heroes.component';
 import { MessageComponent } from './message/message.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
@@ -19,7 +19,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
